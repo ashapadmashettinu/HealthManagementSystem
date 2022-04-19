4f40dc862f46a54052a5b035fa75ac878dbbcea1
Asha Padmashetti <padmashetti.a@northeastern.edu>
2022-04-19 13:11:57 -0400

    fixed error

--- RegisterDoctor.sql
+++ RegisterDoctor.sql
@@ -1,38 +1,64 @@
-CREATE OR ALTER PROCEDURE RegisterDoctor(
-	@FirstName as varchar(255),
-	@LastName as varchar(255),
-	@DOB as date,
-    @password as varchar(255), 
-    @Street varchar(255),
-	@State as varchar(255),
-	@City as varchar(255),
-	@ZipCode as int,
-	@PhoneNumber as int,
-    @emailid as VARCHAR(255) = null,
-    @addressLine2 as VARCHAR(255) = null
-    )
-AS BEGIN
+-- EXECUTE [dbo].[RegisterDoctor]
+-- 	'Asha'
+-- 	,'Padmashetti'
+-- 	,'1994-12-01'
+-- 	,'124abert'
+--     ,'Street 13'
+--   ,'MA'
+--   ,'Boston'
+--   ,01223
+--   ,8989888384
+--   ,'a.p@gmail.com'
 
-DECLARE @output VARCHAR(20);
-DECLARE @UserID INT; -- Varchar(255)
--- Checks if user exists and skips the insertion. 
+CREATE OR ALTER PROCEDURE RegisterDoctor(
+    @FirstName as varchar(255),
+    @LastName as varchar(255),
+    @DOB as date,
+    @password as varchar(255),
+    @Street varchar(255),
+    @State as varchar(255),
+    @City as varchar(255),
+    @ZipCode as int,
+    @PhoneNumber as bigint,
+    @emailid as VARCHAR(255) = null,
+    @addressLine2 as VARCHAR(255) = null,
+    @country as VARCHAR(255) = null
+)
+AS
+BEGIN TRY  
+    DECLARE @output VARCHAR(20);
+    DECLARE @UserID INT;
+    -- Varchar(255)
+    -- Checks if user exists and skips the insertion. 
     DECLARE @AddressID int;
     DECLARE @RoleId int;
-    EXECUTE dbo.INSERT_UPDATE_ADDRESS @Street, @State, @City, @ZipCode, @PhoneNumber, @emailid, @addressLine2, @AddressID output
+    EXECUTE dbo.INSERT_UPDATE_ADDRESS @Street, @State, @City, @ZipCode, @PhoneNumber, @emailid, @addressLine2, @Country, null, @AddressID output
 
-    INSERT INTO DoctorInformation(FirstName, LastName, DOB, AddressID) 
-    values(@FirstName, @LastName, @DOB, @AddressID)
+    IF ISNULL(@AddressID, '') != ''
+    BEGIN
+        INSERT INTO DoctorInformation
+            (FirstName, LastName, DOB, AddressID)
+        values(@FirstName, @LastName, @DOB, @AddressID)
 
-    SET @UserID = SCOPE_IDENTITY()
+        SET @UserID = SCOPE_IDENTITY()
 
-    SELECT @RoleId = RoleId from RolesLookup where RoleType = 'Patient';
+        SELECT @RoleId = RoleId
+        from RolesLookup
+        where RoleType = 'Doctor';
 
-    INSERT INTO LoginInformation(UserID, password ,RoleID) VALUES(@UserID, EncryptByKey(Key_GUID(N'TestSymmetricKey'), @password) , @RoleId)
+        OPEN SYMMETRIC KEY HMSSymmetricKey
+        DECRYPTION BY CERTIFICATE HMSCertificate;
 
-    --INSERT INTO LoginSessions(UserID, Time)VALUES(@UserID, getDate())
+        INSERT INTO LoginInformation
+            (UserID, [password] ,RoleID)
+        VALUES(@UserID, EncryptByKey(Key_GUID('HMSSymmetricKey'), @password) , @RoleId)
+        CLOSE SYMMETRIC KEY HMSSymmetricKey;
+        --INSERT INTO LoginSessions(UserID, Time)VALUES(@UserID, getDate())
 
-    SET @output =  @UserID; 
-
-select @output
-
-END
+        SET @output =  @UserID;
+    END
+     select @output
+END TRY  
+BEGIN CATCH  
+    SELECT ERROR_MESSAGE() AS ErrorMessage;  
+END CATCH;  
--- RegisterPatient.sql
+++ RegisterPatient.sql
@@ -1,68 +1,92 @@
 -- --to execute
-EXECUTE [dbo].[RegisterPatient]
-    4356
-	,'Asha'
-	,'Padmashetti'
-	,'1994-12-01'
-	,'1234qwert'
-    ,'Street 13'
-  ,'MA'
-  ,'Boston'
-  ,01223
-  ,8989888384
-  ,'a.p@gmail.com'
+-- EXECUTE [dbo].[RegisterPatient]
+--     434333456
+-- 	,'Asha'
+-- 	,'Padmashetti'
+-- 	,'1994-12-01'
+-- 	,'1234qwert'
+--     ,'Street 13'
+--   ,'MA'
+--   ,'Boston'
+--   ,01223
+--   ,8989888384
+--   ,'a.p@gmail.com'
 
 CREATE OR ALTER PROCEDURE RegisterPatient(
     @VerificationID as int,
-	@FirstName as varchar(255),
-	@LastName as varchar(255),
-	@DOB as DATE,
-    @password as varchar(255), 
+    @FirstName as varchar(255),
+    @LastName as varchar(255),
+    @DOB as DATE,
+    @password as varchar(255),
     @Street varchar(255),
-	@State as varchar(255),
-	@City as varchar(255),
-	@ZipCode as int,
-	@PhoneNumber as BigInt,
+    @State as varchar(255),
+    @City as varchar(255),
+    @ZipCode as int,
+    @PhoneNumber as BigInt,
     @emailid as VARCHAR(255) = null,
     @PharmacyID int =null,
     @AllergyId int =null
-    )
-AS BEGIN
+)
+AS
+BEGIN
 
-DECLARE @output VARCHAR(20);
-DECLARE @UserID INT; -- Varchar(255)
--- Checks if user exists and skips the insertion. 
+BEGIN TRY  
+  
+    DECLARE @output VARCHAR(20);
+    DECLARE @UserID INT;
+    -- Varchar(255)
+    -- Checks if user exists and skips the insertion. 
 
-IF EXISTS(SELECT 1  FROM PatientPersonalInformation WHERE VerificationID = @VerificationID)
+    IF EXISTS(SELECT 1
+    FROM PatientPersonalInformation
+    WHERE VerificationID = @VerificationID)
     BEGIN
-    --select 'hetg 1' + @VerificationID
-    SELECT @UserID = PatientID  FROM PatientPersonalInformation WHERE VerificationID = @VerificationID
-    SET @output = @UserID
+        --select 'hetg 1' + @VerificationID
+        SELECT @UserID = PatientID
+        FROM PatientPersonalInformation
+        WHERE VerificationID = @VerificationID
+        SET @output = @UserID
     END
 ELSE
 BEGIN
-    DECLARE @AddressID int;
-    DECLARE @RoleId int;
-    EXECUTE dbo.INSERT_UPDATE_PATIENT_ADDRESS @Street, @State, @City, @ZipCode, @PhoneNumber, @emailid, null, @AddressID output
-    
-    IF ISNULL(@AddressID,'') != ''
+        DECLARE @AddressID int;
+        DECLARE @RoleId int;
+        EXECUTE dbo.INSERT_UPDATE_PATIENT_ADDRESS @Street, @State, @City, @ZipCode, @PhoneNumber, @emailid, null, @AddressID output
+
+        IF ISNULL(@AddressID,'') != ''
     BEGIN
-    INSERT INTO PatientPersonalInformation(VerificationID, FirstName, LastName, DOB, AddressID) 
-    values(@VerificationID, @FirstName, @LastName, @DOB, @AddressID)
+            INSERT INTO PatientPersonalInformation
+                (VerificationID, FirstName, LastName, DOB, AddressID)
+            values(@VerificationID, @FirstName, @LastName, @DOB, @AddressID)
 
-    SET @UserID = SCOPE_IDENTITY()
+            SET @UserID = SCOPE_IDENTITY()
 
-    SELECT @RoleId = RoleId from RolesLookup where RoleType = 'Patient';
+            SELECT @RoleId = RoleId
+            from RolesLookup
+            where RoleType = 'Patient';
 
-    INSERT INTO LoginInformation(UserID, password ,RoleID) VALUES(@UserID, EncryptByKey(Key_GUID(N'TestSymmetricKey'), @password) , @RoleId)
+            OPEN SYMMETRIC KEY HMSSymmetricKey
+            DECRYPTION BY CERTIFICATE HMSCertificate;
 
-    --INSERT INTO LoginSessions(UserID, Time)VALUES(@UserID, getDate())
+            INSERT INTO LoginInformation
+                (UserID, password ,RoleID)
+            VALUES(@UserID, EncryptByKey(Key_GUID(N'HMSSymmetricKey'), @password) , @RoleId)
 
-    -- EXECUTE Allergy, Wishlist, Insurance
+            CLOSE SYMMETRIC KEY HMSSymmetricKey;
 
-    SET @output =  @UserID; 
-    END
-END;
-select @output as UserID
+            --INSERT INTO LoginSessions(UserID, Time)VALUES(@UserID, getDate())
 
+            -- EXECUTE Allergy, Wishlist, Insurance
+
+            SET @output =  @UserID;
+        END
+    END;
+    select @output as UserID
+
+END TRY  
+BEGIN CATCH  
+    
+    SELECT ERROR_MESSAGE() AS ErrorMessage;  
+
+END CATCH; 
 END

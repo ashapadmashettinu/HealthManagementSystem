88c4bc907d4fe1b7fd3112c6f2914d618163e24a
Asha Padmashetti <padmashetti.a@northeastern.edu>
2022-04-19 19:08:16 -0400

    Getdoctors function

---
+++ GetDoctorsByNameAndSpecialization.sql
@@ -0,0 +1,24 @@
+-- -- Example  of Execution
+-- select * from  uf_GetDoctorsByNameAndSpecialization('General', 'Ash')
+
+CREATE or alter FUNCTION uf_GetDoctorsByNameAndSpecialization  
+(@specialization  as varchar(255),
+ @doctorName as varchar(255)) 
+RETURNS @table TABLE
+(
+    DoctorID int,
+    FIRSTName varchar(255),
+    LastName varchar(255),
+    SpecializationId int,
+    SpecializationName VARCHAR(255) 
+)
+AS BEGIN
+    INSERT into @table
+    SELECT d.DoctorID, ds.SpecializationID, FIRSTName, LASTName, SpecializationName
+        FROM DoctorInformation d
+        JOIN DoctorSpecialization ds on d.DoctorID = ds.DoctorID
+        JOIN Specialization s on s.SpecializationID = ds.SpecializationID
+        WHERE FirstName like  @doctorName +'%' or LastName like  @doctorName +'%'
+        ORDER BY FirstName, LastName;
+        return 
+END

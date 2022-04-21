e44b349f148f83d48c981527b76eb38808d6bf84
Asha Padmashetti <padmashetti.a@northeastern.edu>
2022-04-21 02:09:19 -0400

    trigger for appointments

--- LoginVerification.sql
+++ LoginVerification.sql
@@ -4,7 +4,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
--- execute Login_Verification 2,'1234qwert'
+-- execute Login_Verification 1001,'124abert'
 
 CREATE or ALTER PROCEDURE Login_Verification
     (
@@ -24,10 +24,13 @@ BEGIN
     WHERE USERID = @UserID and DecryptByKey(password) =@password)
     BEGIN
 
+        INSERT INTO LoginSessions(UserID, SessionTime) Values(@UserID, getDate())
+
         select UserId, p.FirstName, P.LastName, P.DOB
-        from LoginInformation l join PatientPersonalInformation p on p.PatientId = l.UserID
+        from LoginInformation l join PatientPersonalInformation p on p.PatientId = l.UserID where USERId =@UserID
+
     END
+    ELSE
+        SELECT Null as [User]
     CLOSE SYMMETRIC KEY HMSSymmetricKey;
 END
-
-
